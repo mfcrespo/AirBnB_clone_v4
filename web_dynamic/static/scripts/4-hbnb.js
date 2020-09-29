7e3eb54485ca4f003e504ca89482475688510998
llanojs <juanllano93@gmail.com>
2020-09-29 00:15:46 -0500

    add script 4

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -52,33 +52,34 @@ $( document ).ready( function() {
 	});
 
 
-	$('BUTTON').click(() => {
-		$.ajax({
-			type: 'POST',
-			url: 'http://localhost:5001/api/v1/places_search/',
-			data:'{}',
-			dataType: 'json',
-			contentType: 'application/json',
-            data: JSON.stringify({ amenities: Object.keys(amenities) }),
-			success: function (data) {
-	          for (const place of data) {
-				 const _place = `<article>
-				                    <div class="title_box">
-					                   <h2>${place.name}</h2>
-						               <div class="price_by_night">$${place.price_by_night}</div>
-							        </div>
-								    <div class="information">
-									   <div class="max_guest">${place.max_guest} Guests</div>
-									   <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-			                           <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-				                    </div>
-					                <div class="description">${place.description}</div>
-						         </article>`;
-					$('section.places').append(_place);
-				}
-			}
-		});
-	});
+  $('button').click(() => {
+    $.ajax({
+      type: 'POST',
+      // localhost instead of 0.0.0.0 to work on windows
+      url: 'http://localhost:5001/api/v1/places_search/',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      dataType: 'json',
+      contentType: 'application/json',
+      success: (data) => {
+        $('SECTION.places').empty();
+        for (const place of data) {
+          const template = `<article>
+                               <div class="title_box">
+                                  <h2>${place.name}</h2>
+                                  <div class="price_by_night">$${place.price_by_night}</div>
+                               </div>
+                               <div class="information">
+                                  <div class="max_guest">${place.max_guest} Guests</div>
+                                  <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                                  <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                               </div>
+                               <div class="description">${place.description}</div>
+                            </article>`;
+          $('SECTION.places').append(template);
+        }
+      }
+    });
+  });
 
 
 });
