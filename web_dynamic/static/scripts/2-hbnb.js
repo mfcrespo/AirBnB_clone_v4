53ccb9e1421502a1f5ed68be66dd74c58ef4483f
llanojs <juanllano93@gmail.com>
2020-09-28 21:47:35 -0500

    add script 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,27 @@
+$( document ).ready( function() {
+	const amenityList = [];
+	$('input[type=checkbox]').css("margin-left", "10px")
+	$('input[type=checkbox]').click( function() {
+		if ($(this).prop('checked')) {
+			amenityList.push($(this).attr('data-name'));
+			console.log(amenityList);
+   		}
+    	else {
+			const index = amenityList.indexOf($(this).attr('data-name'));
+			amenityList.splice(index, 1);
+    	}
+		$('.amenities h4').text(amenityList.join(', '));
+	});
+
+	$.ajax({
+		url: 'http://localhost:5001/api/v1/status/',
+		dataType: 'text',
+		success: function (data) {
+			if (data.status === 'OK') {
+				$('#api_status').addClass('available');
+			} else {
+				$('#api_status').removeClass('available');
+			}
+		}
+	}):
+});
